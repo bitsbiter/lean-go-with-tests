4d6ac80c63166dbf1db521ce9a94de32b79e6de5
Daan Hermans <daan@thisisdevelopment.nl>
2024-01-23 14:59:01 +0100

    adjusted the price checker so it only looks for even cheaper prices than the last one and exits the program when it found free chicken

--- go-routines-and-channels/priceCheck.go
+++ go-routines-and-channels/priceCheck.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
-const MAX_CHICKEN_PRICE float32 = 3
+// everything above this price is not a deal
+const MAX_CHICKEN_PRICE float32 = 10
+
+var lowestChickenPrice float32
 
 func main() {
 	var chickenChannel = make(chan map[string]float32)
 	var websites = []string{"kip.nl", "kip.be", "kip.com"}
+
+	lowestChickenPrice = MAX_CHICKEN_PRICE
 
 	for i := range websites {
 		go checkChickenPrices(websites[i], chickenChannel)
@@ -24,7 +30,8 @@ func checkChickenPrices(website string, chickenChannel chan map[string]float32) 
 		time.Sleep(time.Second * 1)
 		var chickenPrice = rand.Float32() * 20
 
-		if chickenPrice <= MAX_CHICKEN_PRICE {
+		if chickenPrice <= lowestChickenPrice {
+			lowestChickenPrice = chickenPrice
 			chickenChannel <- map[string]float32{website: chickenPrice}
 		}
 	}
@@ -35,6 +42,11 @@ func sendMessage(chickenChannel chan map[string]float32) {
 		result := <-chickenChannel
 
 		for website, price := range result {
+			if price < 0.00 {
+				fmt.Printf("\nFound free chicken! at: %s", website)
+				os.Exit(0)
+			}
+
 			fmt.Printf("\nFound a deal on chicken at: %s, for %.2f euro", website, price)
 		}
 	}
